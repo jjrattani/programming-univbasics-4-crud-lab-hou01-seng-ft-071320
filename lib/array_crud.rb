b4abbcfbf6937bd2a9e10038b47cebb5f15646f9
Jessica  Rattani <jjrattani@gmail.com>
2020-06-26 18:15:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,8 +8,7 @@ end
 
 def add_element_to_end_of_array (array, element)
   public = ["chilly", "cheese", "bowl", "lemon"]
-  public.push("sauce")
-  p public
+  element.push("sauce")
 end
 
 def add_element_to_start_of_array(array, element)
